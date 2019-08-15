d771950cc8611ffbd7837109e1b13f63e145f07a
Matthew Zapata <matthew.zapata21@gmail.com>
2019-08-14 21:24:32 -0500

    Basic print statements.

---
+++ lookup_program.py
@@ -0,0 +1,4 @@
+print('Welcome to the Pet Microchip Lookup!')
+print("""Please enter the microchip number below. Please look at the microchip number carefully. 
+Check that number 1's are not lowercase l's, etc.""")
+microchip_number = input('Microchip number: ')
