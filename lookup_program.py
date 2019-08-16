f6087e1c434d2b023389015001b462bf6d0ad7d7
Matthew Zapata <matthew.zapata21@gmail.com>
2019-08-16 15:19:37 -0500

    Take slice and compare to microchip chart.

--- lookup_program.py
+++ lookup_program.py
@@ -1,9 +1,19 @@
-import sleep
+from time import sleep
 
 print('Welcome to the Pet Microchip Lookup!')
-print("""Please enter the microchip number below. Please look at the microchip number carefully. 
+sleep(2)
+print("""Enter the microchip number below. Please look at the microchip number carefully. 
 Check that number 1's are not lowercase l's, etc.""")
+sleep(5)
 microchip_number = input('Microchip number: ')
 
 length_of_number = len(microchip_number)
 print(f'Microchip number contains {length_of_number} characters.')
+
+first_three_digits = microchip_number[:3]
+if first_three_digits == '981':
+    print('Datamars, PetLink')
+elif first_three_digits == '985':
+    print('HomeAgain')
+elif first_three_digits == '977':
+    print('AVID')
