8977dc9c3f43f6cf0015c9ca86c9dbe59d9ca9bc
Matthew Zapata <matthew.zapata21@gmail.com>
2019-08-16 15:09:31 -0500

    Check microchip number length.

--- lookup_program.py
+++ lookup_program.py
@@ -1,4 +1,9 @@
+import sleep
+
 print('Welcome to the Pet Microchip Lookup!')
 print("""Please enter the microchip number below. Please look at the microchip number carefully. 
 Check that number 1's are not lowercase l's, etc.""")
 microchip_number = input('Microchip number: ')
+
+length_of_number = len(microchip_number)
+print(f'Microchip number contains {length_of_number} characters.')
