f136a4b28047f9bb08f65a7484ea44464db28f69
Matthew Zapata <matthew.zapata21@gmail.com>
2019-08-17 09:59:23 -0500

    Start implementing regex.

--- lookup_program.py
+++ lookup_program.py
@@ -1,4 +1,5 @@
 from time import sleep
+import re
 
 print('Welcome to the Pet Microchip Lookup!')
 sleep(2)
@@ -11,9 +12,20 @@ length_of_number = len(microchip_number)
 print(f'Microchip number contains {length_of_number} characters.')
 
 first_three_digits = microchip_number[:3]
-if first_three_digits == '981':
-    print('Datamars, PetLink')
-elif first_three_digits == '985':
-    print('HomeAgain')
-elif first_three_digits == '977':
-    print('AVID')
+
+if length_of_number == 15:
+    if microchip_number[:8] == '98101000':
+        print('Banfield')
+    elif first_three_digits == '981':
+        print('Datamars, PetLink')
+    elif first_three_digits == '985':
+        print('HomeAgain')
+    elif first_three_digits == '977':
+        print('AVID')
+    elif first_three_digits == '982':
+        print('24 PetWatch')
+    elif first_three_digits == '956':
+        print('AKC Reunite')
+elif length_of_number == 10:
+    if re.match(r'4\d+', microchip_number):
+        print('HomeAgain')
