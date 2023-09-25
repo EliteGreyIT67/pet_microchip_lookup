841b1b31861a50b673fffcbeab4b14763b1f33d1
EliteGreyIT <EliteGreyIT67@users.noreply.github.com>
2023-09-24 22:11:58 -0400

    Update lookup_program.py
    The main changes I made were:
    
    • I added a print_welcome() function to print the welcome message once, instead of repeating it at the beginning of the loop.
    
    • I added a print_results() function to print the results of the microchip lookup, instead of repeating that code in the loop.
    
    • I moved the logic for determining the microchip brand into a separate function,

--- lookup_program.py
+++ lookup_program.py
@@ -4,20 +4,40 @@ import re
 keep_going = True
 
 
-print('Welcome to the Pet Microchip Lookup!')
-sleep(2)
-print("""Enter the microchip number below. Please look at the microchip number carefully. 
-Check that number 1's are not lowercase l's, etc.""")
-sleep(5)
-
-while keep_going:
-    microchip_number = input('Microchip number (or Q to quit): ')
-    if microchip_number == 'Q':
-        break
-
-    length_of_number = len(microchip_number)
-    print(f'Microchip number contains {length_of_number} characters.')
+def print_welcome():
+    print('Welcome to the Pet Microchip Lookup!')
     sleep(2)
+    print("""Enter the microchip number below. Please look at the microchip number carefully.
+Check that number 1's are not lowercase l's, etc.""")
+    sleep(5)
+
+
+def print_results(microchip_number, brand):
+    print(f"The brand of your pet's microchip is {brand}.")
+    sleep(1)
+
+    if brand in ['HomeAgain', 'Banfield']:
+        manufacturer = microchip_brands[brand]["Manufacturer_" + str(len(microchip_number)) + "_digit"]
+    else:
+        manufacturer = microchip_brands[brand]["Manufacturer"]
+
+    print(f'The manufacturer is {manufacturer}.')
+    sleep(1)
+
+    if brand == 'Datamars, PetLink':
+        frequency = microchip_brands[brand]["Frequency"]
+    else:
+        frequency = microchip_brands[brand]["Frequency_" + str(len(microchip_number)) + "_digit"]
+
+    print(f'The chip\'s frequency is {frequency}.')
+    sleep(1)
+
+    print(f"Please call {microchip_brands[brand]['Contact Phone Number']} for more information regarding your pet\'s microchip.")
+    sleep(2)
+
+
+def get_microchip_brand(microchip_number):
+    length_of_number = len(microchip_number)
 
     first_three_digits = microchip_number[:3]
 
@@ -52,53 +72,27 @@ while keep_going:
         sleep(1)
         print('Please double check that the correct characters were inputted.')
         sleep(2)
-        continue
+        return None
+
+    return brand
 
 
-    microchip_brands = {'Datamars, PetLink':{'Manufacturer':'Datamars', 
-                                            'Frequency':'134.2 kHz', 
-                                            'Contact Phone Number':'1-877-738-5465'},
-                        'HomeAgain':{'Manufacturer_15_digit':'Destron Fearing',
-                                    'Manufacturer_10_digit':'Digital Angel',
-                                    'Frequency_15_digit':'134.2 kHz',
-                                    'Frequency_10_digit':'125 kHz',
-                                    'Contact Phone Number':'1-888-466-3242'},
-                        'AVID':{'Manufacturer':'AVID',
-                                'Frequency_9_digit':'125 kHz', 
-                                'Frequency_10_digit':'125 kHz', 
-                                'Frequency_15_digit':'134.2 kHz', 
-                                'Contact Phone Number':'1-800-336-2843'}, 
-                        '24 PetWatch':{'Manufacturer':'Allflex', 
-                                        'Frequency_15_digit':'134.2 kHz',
-                                        'Frequency_10_digit':'125 kHz', 
-                                        'Contact Phone Number':'1-866-597-2424'}, 
-                        'AKC Reunite':{'Manufacturer':'Trovan', 
-                                        'Frequency_15_digit':'134.2 kHz', 
-                                        'Frequency_10_digit':'128 kHz', 
-                                        'Contact Phone Number':'1-800-252-7894'}, 
-                        'Banfield':{'Manufacturer_15_digit':'Datamars', 
-                                    'Manufacturer_10_digit':'Unknown', 
-                                    'Frequency_15_digit':'134.2 kHz', 
-                                    'Frequency_10_digit':'125 kHz', 
-                                    'Contact Phone Number':'1-877-567-8738'}}
+if __name__ == '__main__':
+    print_welcome()
 
-    print(f"The brand of your pet's microchip is {brand}.")
-    sleep(1)
-    if brand in ['HomeAgain', 'Banfield']:
-        print(f'The manufacturer is {microchip_brands[brand]["Manufacturer_" + str(length_of_number) + "_digit"]}.')
-    else:
-        print(f'The manufacturer is {microchip_brands[brand]["Manufacturer"]}.')
-    sleep(1)
-    if brand == 'Datamars, PetLink':
-        print(f'The chip\'s frequency is {microchip_brands[brand]["Frequency"]}.')
-    else:
-        print(f'The chip\'s frequency is {microchip_brands[brand]["Frequency_" + str(length_of_number) + "_digit"]}.')
-    sleep(1)
-    print(f"Please call {microchip_brands[brand]['Contact Phone Number']} for more information regarding your pet\'s microchip.")
-    sleep(2)
+    while keep_going:
+        microchip_number = input('Microchip number (or Q to quit): ')
+        if microchip_number == 'Q':
+            break
 
-    still_keep_going = input('Do you have another number to lookup? (Y/N) ')
-    if still_keep_going == 'N':
-        break
+        brand = get_microchip_brand(microchip_number)
+        if brand is None:
+            continue
 
-print('Thank you for using the Pet Microchip Lookup.')
+        print_results(microchip_number, brand)
+
+        still_keep_going = input('Do you have another number to lookup? (Y/N) ')
+        if still_keep_going == 'N':
+            break
+
+    print('Thank you for using the Pet Microchip Lookup.')
